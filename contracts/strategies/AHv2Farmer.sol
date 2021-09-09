4ab805c28343fcf0bf4abefa7803e03d3d9cd347
kristian-gro <kristian@gro.xyz>
2021-09-09 17:25:23 +0100

    [strategy] KD - 09.09.21 - Base functionality added (interaction with vault + opening/cloasing/adjusting position during harvest)

--- contracts/strategies/AHv2Farmer.sol
+++ contracts/strategies/AHv2Farmer.sol
@@ -78,6 +78,10 @@ contract AHv2Farmer is BaseStrategy, Constants {
     address public constant homoraBank = address(0xba5eBAf3fc1Fcca67147050Bf80462393814E54B);
 
     uint256 activePosition;
+    uint256 minWant;
+	uint256 reserves;
+	uint256 ilThreshold;
+    uint256 minGovtoSell;
 
 	event newFarmer(address vault, address spell, address router, address lpt, uint256 poolId);
 
@@ -136,7 +140,17 @@ contract AHv2Farmer is BaseStrategy, Constants {
         return "Ahv2 strategy";
     }
 
-    function openPosition(uint256 amount) external onlyAuthorized {
+	function _adjustPosition(uint256[] memory amounts) internal {
+        Amounts memory amt = formatOpen(amounts);
+        IHomora(homoraBank).execute(
+                activePosition,
+                spell,
+                abi.encodeWithSignature(sushiOpen, address(want), weth, amt, poolId)
+        );
+        loadPositionData(IHomora(homoraBank).nextPositionId() - 1, amounts);
+    }
+
+    function openPosition(uint256 amount) internal onlyAuthorized {
         uint256[] memory amounts = uniPrice(amount, address(want));
         Amounts memory amt = formatOpen(amounts);
         IHomora(homoraBank).execute(
@@ -162,24 +176,44 @@ contract AHv2Farmer is BaseStrategy, Constants {
         pos.active = true;
     }
 
-    function closePosition(uint256 positionID) external onlyAuthorized {
-        RepayAmounts memory amt = formatClose(positionID);
+	function panicClose() external onlyAuthorized {
+		closePosition();
+	}
+
+    function closePosition() internal {
+        RepayAmounts memory amt = formatClose();
         IHomora(homoraBank).execute(
                 0,
                 spell,
                 abi.encodeWithSignature(sushiClose, address(want), weth, amt)
         );
-        positionData storage pos = positions[positionID];
+        positionData storage pos = positions[activePosition];
         pos.active = false;
         pos.close = uniPrice(pos.open[0], address(want));
     }
 
-    function adjustPosition(uint256 positionID, uint256 amount) external onlyAuthorized {
-
+    function getPosition() external view returns (
+        address owner,
+        address collToken,
+        uint collId,
+        uint collateralSize
+    ) {
+		return IHomora(homoraBank).getPositionInfo(activePosition);
     }
 
-    function getPosition(uint256 positionId) external view {
+    function getDebt() external view returns (
+        address[] memory tokens,
+        uint[] memory debts
+    ) {
+		return IHomora(homoraBank).getPositionDebts(activePosition);
+    }
 
+    function setMinGovToSell(uint256 _min) external onlyAuthorized {
+                minGovtoSell = _min;
+    }
+
+    function setMinWant(uint256 _minWant) external onlyAuthorized {
+                minWant = _minWant;
     }
 
     function formatOpen(uint256[] memory amounts) internal view returns (Amounts memory amt) {
@@ -189,30 +223,11 @@ contract AHv2Farmer is BaseStrategy, Constants {
         amt.bMin = amounts[1] * (PERCENTAGE_DECIMAL_FACTOR - 1) / PERCENTAGE_DECIMAL_FACTOR;
     }
 
-    function formatClose(uint256  positionId) internal view returns (RepayAmounts memory amt) {
-        positionData storage pd = positions[positionId];
+    function formatClose() internal view returns (RepayAmounts memory amt) {
+        positionData storage pd = positions[activePosition];
         uint256 collateral = pd.collateral;
-        uint256[] memory lpPosition = calcLpPosition(collateral);
         uint256[] memory debts = pd.debt;
-        int256[] memory expected = new int256[](2);
-
-        uint256 amount;
-        for (uint256 i; i < 2; i++) {
-            uint256 positionWithSlippage = lpPosition[i] * (PERCENTAGE_DECIMAL_FACTOR - 10) / PERCENTAGE_DECIMAL_FACTOR;
-            if (i < debts.length) {
-                expected[i] = int256(positionWithSlippage) - int256(debts[i]);
-            } else {
-                expected[i] = int256(positionWithSlippage);
-            }
-        }
-
-        for (uint256 i; i < 2 ; i++) {
-            if (expected[i] < 0) {
-                uint256[] memory change = uniPrice(uint256(expected[i] * -1), getPath(1 - i));
-                expected[1 - i] -= int256(change[1]);
-                expected[i] = 0;
-            } 
-        }
+        int256[] memory expected = _calcWantAvailable(collateral, debts); 
 
         amt.lpTake = collateral;
         amt.bRepay = REPAY;
@@ -260,14 +275,44 @@ contract AHv2Farmer is BaseStrategy, Constants {
     ///     This method is only used to pull out debt if debt ratio has changed.
     /// @return Total assets in want this strategy has invested into underlying vault
     function estimatedTotalAssets() public view override returns (uint256) {
+
+		uint256 valueOfDeposit = valueOfDeposit();
+        // uint256 currentSushi = ... calculate amount of sushi
+        return want.balanceOf(address(this)) + valueOfDeposit; // + currentSushi
     }
 
     function expectedReturn() external view returns (uint256) {
+        uint256 estimateAssets = estimatedTotalAssets();
+
+        uint256 debt = vault.strategies(address(this)).totalDebt;
+        if (debt > estimateAssets) {
+            return 0;
+        } else {
+            return estimateAssets - debt;
+        }
     }
 
-    /// @notice This strategy doesn't realize profit outside of APY from the vault.
-    ///     This method is only used to pull out debt if debt ratio has changed.
-    /// @param _debtOutstanding Debt to pay back
+	function getCurrentPosition() private view returns (uint256 deposit, uint256 borrow) {
+        uint256 deposit = IHomora(homoraBank).getCollateralETHValue(activePosition);
+		uint256 borrow =  IHomora(homoraBank).getBorrowETHValue(activePosition);
+	}
+
+	function setReserves(uint256 newReserves) external {
+        require(newReserves <= 1000, 'setReserves: !newReserves');
+        reserves = newReserves;
+	}
+
+	function setIlThreshold(uint256 newThreshold) external {
+        require(newThreshold <= 1000, 'setIlThreshold: !newThreshold');
+        ilThreshold = newThreshold;
+	}
+
+    function valueOfDeposit() private view returns (uint256) {
+        (uint256 deposits, uint256 borrows) = getCurrentPosition();
+        uint256[] memory value = uniPrice(deposits - borrows, weth);
+        return value[1];
+    }
+		
     function prepareReturn(uint256 _debtOutstanding)
         internal
         override
@@ -277,38 +322,178 @@ contract AHv2Farmer is BaseStrategy, Constants {
             uint256 _debtPayment
         )
     {
+        if (activePosition == 0) {
+            uint256 wantBalance = want.balanceOf(address(this));
+            //no active position
+            _debtPayment = Math.min(wantBalance, _debtOutstanding); 
+            return (_profit, _loss, _debtPayment);
+        }
+
+		// get sushi amount ??
+		// sell sushi amount ??
+
+        uint256 wantBalance = want.balanceOf(address(this));
+
+		// want value of deposit
+		uint256 investedBalance = valueOfDeposit();
+        uint256 balance = investedBalance + wantBalance;
+
+        uint256 debt = vault.strategies(address(this)).totalDebt;
+
+        //Balance - Total Debt is profit
+        if (balance > debt) {
+            _profit = balance - debt;
+
+            if (wantBalance < _profit) {
+                //all reserve is profit                
+                _profit = wantBalance;
+            } else if (wantBalance > _profit + _debtOutstanding) {
+                _debtPayment = _debtOutstanding;
+            } else{
+                _debtPayment = wantBalance - _profit;
+            }
+        } else {
+            _loss = debt - balance;
+            _debtPayment = Math.min(wantBalance, _debtOutstanding);
+        }
+    }
+    
+    function volatilityCheck() public view returns(bool) {
+        if (activePosition == 0) {
+            return false;
+        }
+        uint256[] memory openPrice = positions[activePosition].open;
+        uint256[] memory currentPrice = uniPrice(openPrice[0], address(want));
+        uint256 difference = (openPrice[1] * PERCENTAGE_DECIMAL_FACTOR / currentPrice[1]);
+        if (difference >= ilThreshold) return true;
+        return false;
+
     }
 
-    /// @notice Withdraw amount from yVault/pool
+    function _calcDesiredPosition(uint256 _amount, bool add) private returns (uint256[] memory) {
+        positionData storage pd = positions[activePosition];
+        uint256 collateral = pd.collateral;
+
+        uint256[] memory lpPosition = calcLpPosition(collateral);
+
+        if (add) {
+            uint256[] memory newPosition = uniPrice(_amount - reserves, address(want));
+            for (uint256 i; i < 2; i ++) {
+                newPosition[i] += lpPosition[1-i];
+            }
+            return newPosition;
+        } else {
+            if (_amount < lpPosition[1]) {
+                uint256[] memory repay = uniPrice(_amount, address(want));
+                lpPosition[0] -= repay[1];
+                lpPosition[1] -= _amount;
+                return lpPosition;
+            }
+        }
+    }
+
+    function _calcWantAvailable(uint256 collateral, uint256[] memory debts) private view returns (int256[] memory) {
+        uint256[] memory lpPosition = calcLpPosition(collateral);
+        int256[] memory expected = new int256[](2);
+
+        uint256 amount;
+        for (uint256 i; i < 2; i++) {
+            uint256 positionWithSlippage = lpPosition[i] * (PERCENTAGE_DECIMAL_FACTOR - 10) / PERCENTAGE_DECIMAL_FACTOR;
+            if (i < debts.length) {
+                expected[i] = int256(positionWithSlippage) - int256(debts[i]);
+            } else {
+                expected[i] = int256(positionWithSlippage);
+            }
+        }
+
+        for (uint256 i; i < 2 ; i++) {
+            if (expected[i] < 0) {
+                uint256[] memory change = uniPrice(uint256(expected[i] * -1), getPath(1 - i));
+                expected[1 - i] -= int256(change[1]);
+                expected[i] = 0;
+            } 
+        }
+        return expected;
+    }
+
     /// @param _amount Expected amount to withdraw
     function _withdrawSome(uint256 _amount) internal returns (uint256) {
+        uint256[] memory newPosition = _calcDesiredPosition(_amount, false);
+        if (newPosition[0] > 0) {
+            _adjustPosition(newPosition);
+        }
     }
 
-    /// @notice Used when emergency stop has been called to empty out strategy
     /// @param _amountNeeded Expected amount to withdraw
     function liquidatePosition(uint256 _amountNeeded)
         internal
         override
-        returns (uint256 _liquidatedAmount, uint256 _loss)
+        returns (uint256 _amountFreed, uint256 _loss)
     {
+        uint256 _balance = want.balanceOf(address(this));
+        uint256 assets = valueOfDeposit(); 
+
+        uint256 debtOutstanding = vault.debtOutstanding();
+
+        if(debtOutstanding > assets){
+            _loss = debtOutstanding - assets;
+        }
+
+        if (assets < _amountNeeded) {
+
+			if (activePosition != 0) {
+				closePosition();
+            }
+
+            _amountFreed = Math.min(_amountNeeded, want.balanceOf(address(this)));
+           
+        } else {
+            if (_balance < _amountNeeded) {
+                _withdrawSome(_amountNeeded - _balance);
+
+                //overflow error if we return more than asked for
+                _amountFreed = Math.min(_amountNeeded, want.balanceOf(address(this)));
+            }else{
+                _amountFreed = _amountNeeded;
+            }
+        }
     }
 
-    /// @notice Used to invest any assets sent from the vault during report
     /// @param _debtOutstanding Should always be 0 at this point
     function adjustPosition(uint256 _debtOutstanding) internal override {
+		//emergency exit is dealt with in prepareReturn
+        if (emergencyExit) {
+            return;
+        }
+
+        uint256 _activePosition = activePosition;
+        if (_activePosition > 0 && volatilityCheck()) {
+            closePosition();
+            return;
+        }
+
+        //we are spending all our cash unless we have debt outstanding
+        uint256 _wantBal = want.balanceOf(address(this));
+        if(_wantBal < _debtOutstanding){
+			_withdrawSome(_debtOutstanding - _wantBal);
+            return;
+        }
+        if (_wantBal > minWant + reserves) {
+            if (activePosition == 0 ) {
+                openPosition(_wantBal - reserves);
+            } else {
+                uint256[] memory newPosition = _calcDesiredPosition(_wantBal - reserves, true);
+                _adjustPosition(newPosition);
+            }
+        }
     }
 
     /// @notice Tokens protected by strategy - want tokens are protected by default
     function protectedTokens() internal view override returns (address[] memory) {
     }
 
-    /// @notice Estimated total assets of strategy
-    /// @param diff Calc token amounts (curve) underreports total amount, to counteract this
-    ///     we add initial difference from last harvest to estimated total assets,
-    function _estimatedTotalAssets(bool diff) private view returns (uint256) {
-    }
-
     function prepareMigration(address _newStrategy) internal override {
-
+        require(activePosition == 0, 'prepareMigration: active position');
+        //want.safeTransfer(want.balanceOf(address(this)), _newStrategy);
     }
 }
