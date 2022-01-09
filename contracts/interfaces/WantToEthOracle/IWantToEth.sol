6f97fbd8ffc8af44f97c4b99f277f28c77d92a81
kristian-gro <kristian@gro.xyz>
2022-01-09 16:53:37 +0000

    [ref] KD - 09.01.22 - Changing strategy repo for new layout - main being latest base from which to develop strategies from

--- contracts/interfaces/WantToEthOracle/IWantToEth.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPLv3
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.6.12;
-
-interface IWantToEth {
-    function wantToEth(uint256 input) external view returns (uint256);
-
-    function ethToWant(uint256 input) external view returns (uint256);
-}
