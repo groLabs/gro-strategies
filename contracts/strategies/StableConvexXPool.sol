6181593330ecce52e9dd14d0e6749803cdcacb40
kongTheDegen <kong@swixdao.com>
2022-04-10 11:40:06 +0200

    correcting address format

--- contracts/strategies/StableConvexXPool.sol
+++ contracts/strategies/StableConvexXPool.sol
@@ -68,7 +68,7 @@ contract StableConvexXPool is BaseStrategy {
 
     address public constant CVX = address(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
     address public constant CRV = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
-    address public constant SPELL = address(0x090185f2135308bad17527004364ebcc2d37e5f6);
+    address public constant SPELL = address(0x090185f2135308BaD17527004364eBcC2D37e5F6);
     address public constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     address public constant DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     address public constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
